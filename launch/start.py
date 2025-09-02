7bd1190ab5596bb9b3ceaf2a1bc4dfa980d184d9
Batuhan Beytekin <batuhanbeytekin@gmail.com>
2025-09-02 19:12:38 +0300

    add multi-lidar configs

--- launch/start.py
+++ launch/start.py
@@ -3,8 +3,36 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    rviz_config=get_package_share_directory('hesai_ros_driver')+'/rviz/rviz2.rviz'
     return LaunchDescription([
-        Node(namespace='hesai_ros_driver', package='hesai_ros_driver', executable='hesai_ros_driver_node', output='screen'),
-        Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d',rviz_config])
-    ])
+        Node(
+            namespace='jt16_front',
+            package='hesai_ros_driver',
+            executable='hesai_ros_driver_node',
+            name='hesai_ros_driver_node',
+            output='screen',
+            parameters=[{'config_path': 'config/JT16_front_config.yaml'}]
+        ),
+        Node(
+            namespace='jt16_rear',
+            package='hesai_ros_driver',
+            executable='hesai_ros_driver_node',
+            name='hesai_ros_driver_node',
+            output='screen',
+            parameters=[{'config_path': 'config/JT16_rear_config.yaml'}]
+        ),
+        Node(
+            namespace='ot128',
+            package='hesai_ros_driver',
+            executable='hesai_ros_driver_node',
+            name='hesai_ros_driver_node',
+            output='screen',
+            parameters=[{'config_path': 'config/OT128_config.yaml'}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', get_package_share_directory('hesai_ros_driver') + '/rviz/rviz2.rviz']
+        )
+    ]) 
